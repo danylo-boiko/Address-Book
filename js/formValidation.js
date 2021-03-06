cc15b7998b290bc92c398d9fdaaf357b75bc3e37
Daniel Boyko <55975773+navigati0n@users.noreply.github.com>
2021-03-06 17:49:33 +0200

    Visual refactoring

--- js/formValidation.js
+++ js/formValidation.js
@@ -42,16 +42,16 @@ let validateInputs = (form) => {
 
 let isPhone = (phone) => {
     return /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/.test(phone);
-}
+};
 
 let isFullName = (fullName) => {
     return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(fullName);
-}
+};
 
 let isEmail = (email) => {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
-}
+};
 
 let isAddress = (address) => {
     return /^[a-zA-Z0-9\s,'-]*$/.test(address);
-}
+};
