a9519a7ee15ae4657a2cd005c41c5a7f39dbddf0
Daniel Boyko <55975773+navigati0n@users.noreply.github.com>
2021-03-06 12:07:30 +0200

    Added sorting by field and refactoring

--- js/script.js
+++ js/script.js
@@ -2,42 +2,56 @@ window.onload = () => {
     updateRecordsTable();
 
     //add or update record
-    let addOrUpdateRecordForm = document.getElementById('addOrUpdateRecordForm');
-    addOrUpdateRecordForm.addEventListener('submit', () => {
+    let addOrEditRecordForm = document.getElementById('addOrEditRecordForm');
+    addOrEditRecordForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
         let newRecord = {
-            phone: addOrUpdateRecordForm.phone.value,
-            fullName: addOrUpdateRecordForm.fullName.value,
-            email: addOrUpdateRecordForm.email.value,
-            address: addOrUpdateRecordForm.address.value
+            phone: addOrEditRecordForm.phone.value,
+            fullName: addOrEditRecordForm.fullName.value,
+            email: addOrEditRecordForm.email.value,
+            address: addOrEditRecordForm.address.value
         };
+
+        addOrEditRecordForm.phone.value = '';
+        addOrEditRecordForm.fullName.value = '';
+        addOrEditRecordForm.email.value = '';
+        addOrEditRecordForm.address.value = '';
 
         let serialNewRecord = JSON.stringify(newRecord);
         localStorage.setItem(newRecord.phone, serialNewRecord);
-        location.reload();
+
+        showMainTable();
+        updateRecordsTable();
     });
 
-    //edit record
-    let editLinks = document.querySelectorAll('.editRecord');
-    editLinks.forEach(link => link.addEventListener('click', (event) => {
-        let record = JSON.parse(localStorage.getItem(event.target.parentElement.parentElement.firstChild.innerText));
+    //full name filter
+    let filterFullNameForm = document.getElementById('fullNameFilterForm');
+    filterFullNameForm.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-        addOrUpdateRecordForm.phone.value = record.phone;
-        addOrUpdateRecordForm.fullName.value = record.fullName;
-        addOrUpdateRecordForm.email.value = record.email;
-        addOrUpdateRecordForm.address.value = record.address;
+        let countFilerResult = 0;
+        document.getElementById('recordsList').innerHTML = '';
 
-        showAddNewRecordForm();
-    }));
+        for (let i = 0; i < localStorage.length; i++) {
+            let record = JSON.parse(localStorage.getItem(localStorage.key(i)));
+            if (record.fullName.includes(filterFullNameForm.fullNameFilter.value)) {
+                countFilerResult++;
+                document.getElementById('recordsList').innerHTML +=
+                    '<tr><td>' + record.phone + '</td><td>' + record.fullName + '</td><td>' + record.email + '</td><td>' + record.address +
+                    '</td><td><span class="editRecord">Edit</span> | <span class="deleteRecord">Delete</span></td></tr>';
+            }
+        }
 
-    //delete record
-    let deleteLinks = document.querySelectorAll('.deleteRecord');
-    deleteLinks.forEach(link => link.addEventListener('click', (event) => {
-        localStorage.removeItem(event.target.parentElement.parentElement.firstChild.innerText);
+        updateEditLinks();
+        updateDeleteLinks();
 
-        location.reload();
-    }));
+        if (countFilerResult === 0) {
+            document.getElementById('recordsList').innerHTML = '<tr><td colspan="5">No records with a similar full name.</td></tr>';
+        }
+    });
 
-    document.getElementById('showAddOrUpdateRecordForm').addEventListener('click', showAddNewRecordForm);
+    document.getElementById('showAddOrEditRecordForm').addEventListener('click', showAddOrEditRecordForm);
     document.getElementById('sortByPhone').addEventListener('click', () => sort(0));
     document.getElementById('sortByFullName').addEventListener('click', () => sort(1));
     document.getElementById('sortByEmail').addEventListener('click', () => sort(2));
@@ -46,7 +60,6 @@ window.onload = () => {
 
 let updateRecordsTable = () => {
     document.getElementById('recordsList').innerHTML = '';
-
     if (localStorage.length === 0) {
         document.getElementById('recordsList').innerHTML = '<tr><td colspan="5">You have no records. Why not add a few?</td></tr>';
         return;
@@ -58,10 +71,42 @@ let updateRecordsTable = () => {
             '<tr><td>' + record.phone + '</td><td>' + record.fullName + '</td><td>' + record.email + '</td><td>' + record.address +
             '</td><td><span class="editRecord">Edit</span> | <span class="deleteRecord">Delete</span></td></tr>';
     }
+
+    updateEditLinks();
+    updateDeleteLinks();
 };
 
-let showAddNewRecordForm = () => {
+let updateEditLinks = () => {
+    let addOrUpdateRecordForm = document.getElementById('addOrEditRecordForm');
+    document.querySelectorAll('.editRecord').forEach(link => link.addEventListener('click', (event) => {
+        let record = JSON.parse(localStorage.getItem(event.target.parentElement.parentElement.firstChild.innerText));
+
+        addOrUpdateRecordForm.phone.value = record.phone;
+        addOrUpdateRecordForm.fullName.value = record.fullName;
+        addOrUpdateRecordForm.email.value = record.email;
+        addOrUpdateRecordForm.address.value = record.address;
+
+        showAddOrEditRecordForm();
+    }));
+}
+
+let updateDeleteLinks = () => {
+    document.querySelectorAll('.deleteRecord').forEach(link => link.addEventListener('click', (event) => {
+        localStorage.removeItem(event.target.parentElement.parentElement.firstChild.innerText);
+        updateRecordsTable();
+    }));
+}
+
+let showAddOrEditRecordForm = () => {
     document.getElementById('mainTable').style.display = 'none';
-    document.getElementById('showAddOrUpdateRecordForm').style.display = 'none';
-    document.getElementById('addOrUpdateRecordForm').style.display = 'block';
+    document.getElementById('showAddOrEditRecordForm').style.display = 'none';
+    document.getElementById('fullNameFilterForm').style.display = 'none';
+    document.getElementById('addOrEditRecordForm').style.display = 'block';
 };
+
+let showMainTable = () => {
+    document.getElementById('mainTable').style.display = 'block';
+    document.getElementById('showAddOrEditRecordForm').style.display = 'block';
+    document.getElementById('fullNameFilterForm').style.display = 'block';
+    document.getElementById('addOrEditRecordForm').style.display = 'none';
+}
